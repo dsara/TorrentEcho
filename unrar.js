1bcabcee7438e66c79cca4ee21a58b62dbce1564
darknessgp <darknessgp@gmail.com>
2016-07-12 13:30:45 -0500

    Adding check so that we ensure that we have a rar file before trying to extract it.

--- unrar.js
+++ unrar.js
@@ -2,8 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var rarModule = require('node-unrar');
 
-function Unrar(config) {
-};
+function Unrar(config) {};
 
 // Method for checking if there are rar'd files to uncompress
 Unrar.prototype.HandleFolder = function(folderPath) {
@@ -11,22 +10,26 @@ Unrar.prototype.HandleFolder = function(folderPath) {
     var rarFile = '';
     var files = fs.readdirSync(folderPath);
 
-    for(var i in files) {
-       if(path.extname(files[i]) === ".rar") {
-           rarFile = files[i];
-           break;
-       }
-    }
-    // Construct the rar module with the full rar path
-    var rar = new rarModule(folderPath + "/" + rarFile);
-
-    rar.extract(folderPath, null, function(err) {
-        if (err) {
-            WriteMessage("Error in unrar for " + folderPath + " ERROR: " + err);
-        } else {
-            WriteMessage(rarFile + " extracted");
+    for (var i in files) {
+        if (path.extname(files[i]) === ".rar") {
+            rarFile = files[i];
+            break;
         }
-    });
+    }
+
+    // Ensure we have a rar file to unrar
+    if (rarFile !== '') {
+        // Construct the rar module with the full rar path
+        var rar = new rarModule(folderPath + "/" + rarFile);
+
+        rar.extract(folderPath, null, function(err) {
+            if (err) {
+                WriteMessage("Error in unrar for " + folderPath + " ERROR: " + err);
+            } else {
+                WriteMessage(rarFile + " extracted");
+            }
+        });
+    }
 }
 
 module.exports = Unrar;
