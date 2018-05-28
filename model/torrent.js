aa778a8a32a8af1d5452c05d7c203e9e6386ec51
dsara <dan.steve.sara@gmail.com>
2018-05-27 20:48:58 -0600

    remove old js files

--- model/torrent.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Torrent {
-    constructor(shouldDownload, isDirectory, torrentName, remotePath, downloadLocation) {
-        this.ShouldDownload = shouldDownload;
-        this.IsDirectory = isDirectory;
-        this.RemotePath = remotePath;
-        this.DownloadLocation = downloadLocation;
-        this.Name = torrentName;
-    }
-}
