5008eac71a291e49c0ffbb5d5097807136eb6323
dsara <dan.steve.sara@gmail.com>
2018-06-04 10:00:12 -0600

    added with to title casing

--- app/tools/util.ts
+++ app/tools/util.ts
@@ -156,7 +156,7 @@ export default class Util {
 
   private toTitleCase(e) {
     e = e.replace(/\./g, ' ');
-    const t = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i;
+    const t = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via|with)$/i;
     return e.replace(/([^\W_]+[^\s-]*) */g, function(p, n, r, i) {
       return r > 0 && r + n.length !== i.length && n.search(t) > -1 && i.charAt(r - 2) !== ':' && i.charAt(r - 1).search(/[^\s-]/) < 0
         ? p.toLowerCase()
